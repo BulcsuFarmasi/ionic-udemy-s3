8d573d7f343f973a69d208b41a4b4eb689efe350
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-02 20:55:31 +0200

    add data source

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { Quote } from '../../data/quote.interface';
+import quotes from '../../data/quotes';
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit{
+  public quoteCollection: {category: string, quotes:Quote[], icon:string}[]
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ngOnInit () {
+    this.quoteCollection = quotes;
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
 }
