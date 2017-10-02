3a5ecf215ca43e1fb560b1c1dec38c8daaa58024
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-02 19:40:23 +0200

    add pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { FavoritesPage } from '../pages/favorites/favorites';
 import { HomePage } from '../pages/home/home';
+import { LibraryPage } from '../pages/library/library';
+import { QuotePage } from '../pages/quote/quote';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { SettingsPage } from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    HomePage,
+    LibraryPage,
+    QuotePage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +28,12 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    FavoritesPage,
+    HomePage,
+    LibraryPage,
+    QuotePage,
+    QuotesPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
