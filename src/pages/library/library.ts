3f97f7e9c17cc43480b0ac2add00c90a84c3f7a8
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-03 17:57:54 +0200

    passing data to quotes page

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Quote } from '../../data/quote.interface';
 import quotes from '../../data/quotes';
+import { QuotesPage } from '../quotes/quotes';
 
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit{
-  public quoteCollection: {category: string, quotes:Quote[], icon:string}[]
+  public quoteCollection: {category: string, quotes:Quote[], icon:string}[];
+  public quotesPage = QuotesPage;
 
   ngOnInit () {
     this.quoteCollection = quotes;
