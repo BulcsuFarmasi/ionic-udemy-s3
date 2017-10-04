c7786813d41e70f845cfb7b3310e7242d4470afb
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-04 07:56:39 +0200

    dismissing modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+
+    constructor(private viewController:ViewController){}
+
+    onClose () {
+        this.viewController.dismiss();
+    }
 
 }
