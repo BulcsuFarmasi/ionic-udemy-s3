e4bde79341abed337bc73e97e6a9474f9cb99e4d
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2017-10-04 08:06:03 +0200

    passinng data to the modal

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { NavParams, ViewController } from 'ionic-angular';
 
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
+    public person:string;
+    public text:string;
 
-    constructor(private viewController:ViewController){}
+    constructor(private viewController:ViewController,
+                private navParams:NavParams){}
+
+    ionViewDidLoad () {
+      this.person = this.navParams.get('person');
+      this.text = this.navParams.get('text');
+    }
 
     onClose () {
         this.viewController.dismiss();
